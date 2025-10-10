3ea44e25aeb70b698e8748011d8998a89472a449
George-1999-os <georgeibuchi44@gmail.com>
2025-10-10 17:14:32 +0100

    Add subqueries: avg rating > 4 and users with >3 bookings

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,18 @@
+-- Find all properties where the average rating is greater than 4.0
+SELECT *
+FROM properties
+WHERE id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- Find users who have made more than 3 bookings
+SELECT *
+FROM users u
+WHERE 3 < (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.id
+);
