6dbff8b29edb70b3b1b167b82a9f1166acc8b586
George-1999-os <georgeibuchi44@gmail.com>
2025-10-10 16:48:25 +0100

    Add complex join queries with sample data

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,52 @@
+-- INNER JOIN: Retrieve all bookings and their respective users
+SELECT
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM bookings AS b
+INNER JOIN users AS u
+    ON b.user_id = u.id;
+
+-- LEFT JOIN: Retrieve all properties and their reviews
+SELECT
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location,
+    r.id AS review_id,
+    r.rating,
+    r.comment
+FROM properties AS p
+LEFT JOIN reviews AS r
+    ON p.id = r.property_id;
+
+-- FULL OUTER JOIN: Retrieve all users and bookings (simulated)
+SELECT
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM users AS u
+LEFT JOIN bookings AS b
+    ON u.id = b.user_id
+UNION
+SELECT
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM users AS u
+RIGHT JOIN bookings AS b
+    ON u.id = b.user_id;
